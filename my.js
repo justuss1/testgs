9e86549205727b8e0996b79a5f782dccf7a12262
Justinas Sinkevicius <36824644+justuss1@users.noreply.github.com>
2022-10-07 16:43:57 +0300

    Update my.js

--- my.js
+++ my.js
@@ -1,13 +1,32 @@
+var url = "https://script.google.com/macros/s/AKfycbxUmOu5afiEtrBhBq53QA0npxq2JRN1wgWcFPcs0QW8-TiYx47WFzaEdOTa5cmlGJCoAw/exec"
 
 function testGS() {
-    var url = "https://script.google.com/macros/s/AKfycbxA6VhNEaZfnKZ_53eT9kMgK97e67M8FmjdgN8irVcn16DrrO_JuWoN0gQjatzDm8vOXQ/exec"
-
     fetch(url)
         .then(d => d.json())
         .then(d => {
             document.getElementById("app").textContent = d[0].status;
-
         });
 }
 
 document.getElementById("btn").addEventListener("click", testGS);
+
+
+async function  addGS() {
+
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+        'Content-Type': 'application/json'
+       
+        },
+        redirect: 'follow', // manual, *follow, error
+        body: JSON.stringify({name:"Justinas"}) // body data type must match "Content-Type" header
+  });
+
+    
+
+}
+
+document.getElementById("btn2").addEventListener("click", addGS);
